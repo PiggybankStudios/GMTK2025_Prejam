05114e3eda9110b35c1ab623c7846f6996e8461b
GregRGM <gregmorton.rgm@gmail.com>
2025-05-14 23:30:36 -0700

    Trying out bullet holder

--- GregFPSStuff/Enemies/enemy_attack.gd
+++ GregFPSStuff/Enemies/enemy_attack.gd
@@ -6,12 +6,14 @@ extends Node3D
 @export var damage = 1
 @export var spread: Vector2 = Vector2.ZERO
 @export var range =  1000
-
+# @export var Bullet_Holder:bullet_holder
+@onready var Bullet_Holder= get_node("/root/World/%BulletHolder")
 @onready var Bullet_Point = get_node("%BulletPoint")
 @onready var Debug_Bullet = preload("res://GregFPSStuff/Objects/Spawnable_Objects/hit_debug.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Bullet_Holder = get_node("BulletHolder")
 	pass # Replace with function body.
 
 func _on_timer_timeout():
@@ -26,7 +28,8 @@ func Load_Projectile():
 	_projectile.global_transform.origin = Bullet_Point.global_transform.origin
 	#_projectile.rotate(Vector3.UP,deg_to_rad(Bullet_Point.transform.rotation))
 	#_projectile.body_entered.connect(_on_detection_area_body_entered.bind(_projectile))
-	_projectile._Set_Projectile(damage,spread,range,Bullet_Point)
+	if Bullet_Holder != null:
+		Bullet_Holder._Set_Projectile(damage,spread,range,Bullet_Point)
 
 	
 	#Add_Signal_To_HUD.emit(_projectile)
--- GregFPSStuff/Objects/World/bullet_holder.gd
+++ GregFPSStuff/Objects/World/bullet_holder.gd
@@ -1,5 +1,5 @@
 extends Node3D
-class_name Enemy_Projectile
+class_name bullet_holder
 
 signal Hit_Successful
 
